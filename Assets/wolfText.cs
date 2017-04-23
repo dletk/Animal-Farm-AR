3f0c2f8e182f5e0f530e36167edc5844fa54445a
Giang Duong <gduong@Giangs-MacBook-Pro.local>
2017-04-22 21:57:02 -0500

    Add Buttons

---
+++ Assets/wolfText.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class wolfText : MonoBehaviour {
+
+	public Button textButton;
+	public Text displayText;
+	bool display = false;
+
+	// Use this for initialization
+	void Start() {
+		displayText.text = "";
+		textButton.onClick.AddListener(DisplayText);
+	}
+
+	public void DisplayText() {
+		if (!display) {
+			displayText.text = "Hey! This is a wolf!";
+			Debug.Log ("Touched");
+			display = true;
+		} else {
+			displayText.text = "";
+			display = false;
+		}
+	}
+
+
+//	void OnGUI()
+//	{
+//		if (display) {
+//			// If you've clicked the object, show this button
+//			GUI.Label (new Rect (100, 100, 100, 20), "Hello World");
+//			display = false;
+////			Debug.Log("Touched!");
+//		}
+//	}
+//
+	// Update is called once per frame
+	void Update () {
+
+	}
+			
+}
