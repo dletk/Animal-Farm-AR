3f229fa16fc413b21646141f22de46ba8d3e0479
dletk <dle@macalester.edu>
2017-04-24 21:01:54 -0500

    Add the script for interaction between wolf and tiger

---
+++ Assets/WolfInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfInteraction : MonoBehaviour {
+
+	public Transform tigerPosition;
+	public float distanceToTiger;
+
+	// Use this for initialization
+	void Start () {
+		tigerPosition = GameObject.Find("tiger_idle").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distanceToTiger = Vector3.Distance (gameObject.transform.position, tigerPosition.position);
+		Debug.Log ("-------> Distance: " + distanceToTiger);
+		TextMesh wolfText = GameObject.Find ("WolfText3D").GetComponent<TextMesh> ();
+		if (distanceToTiger <= 10) {
+			wolfText.text = "Found Tiger";
+		} else {
+			wolfText.text = "Hello, Wolf!";
+		}
+	}
+}
