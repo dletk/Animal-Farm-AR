1354c100815ecc877862d3b7b2e9ec568fc66d05
dletk <dle@macalester.edu>
2017-04-25 22:29:03 -0500

    Wolf is able to find tiger and kitten now

--- Assets/WolfInteraction.cs
+++ Assets/WolfInteraction.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class WolfInteraction : MonoBehaviour {
 
-	public Transform tigerPosition;
+	public Transform tigerTransform;
 	public float distanceToTiger;
+	public Transform kittenTransform;
+	public float distanceToKitten;
 
 	// Use this for initialization
 	void Start () {
-		tigerPosition = GameObject.Find("tiger_idle").GetComponent<Transform> ();
+		tigerTransform = GameObject.Find ("tiger_idle").GetComponent<Transform> ();
+		kittenTransform = GameObject.Find ("kitten").GetComponent<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		distanceToTiger = Vector3.Distance (gameObject.transform.position, tigerPosition.position);
+		distanceToTiger = Vector3.Distance (gameObject.transform.position, tigerTransform.position);
+		distanceToKitten = Vector3.Distance (gameObject.transform.position, kittenTransform.position);
 //		Debug.Log ("-------> Distance: " + distanceToTiger);
 		TextMesh wolfText = GameObject.Find ("WolfText3D").GetComponent<TextMesh> ();
-		if (distanceToTiger <= 5) {
-			wolfText.text = "Found Tiger";
+		string potentialWarning;
+		if (distanceToTiger < distanceToKitten) {
+			potentialWarning = "Found Tiger";
+		} else {
+			potentialWarning = "Found Kitten";
+		}
+		if (distanceToTiger <= 5 || distanceToKitten <= 5) {
+			wolfText.text = potentialWarning;
 		} else {
 			wolfText.text = "";
 		}
