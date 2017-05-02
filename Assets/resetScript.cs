1bf72937e6489ce104b5726c5b467b65ea639cb1
giangduong36 <gduong@macalester.edu>
2017-05-02 13:28:41 -0500

    Add Reset Button

---
+++ Assets/resetScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class resetScript : MonoBehaviour {
+
+//	public GameObject gameObj;
+	// Use this for initialization
+	public Button resetButton;
+
+	void Start () {
+		resetButton.onClick.AddListener(ResetPos);
+	}
+
+	void ResetPos(){
+			GameObject.Find("Wolf").transform.position = GameObject.Find("WolfImageTarget").GetComponent<Transform>().position;
+			GameObject.Find("Tiger").transform.position = GameObject.Find("TigerImageTarget").GetComponentInParent<Transform>().position;
+			GameObject.Find("Kitten").transform.position = GameObject.Find("KittenImageTarget").GetComponentInParent<Transform>().position;
+			GameObject.Find("Horse").transform.position = GameObject.Find("HorseImageTarget").GetComponentInParent<Transform>().position;
+			Debug.Log ("Reset!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
